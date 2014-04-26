5ec4897112975e47ce1db4716421c30de5b60547
Sanctumed <riko_ophorst@hotmail.com>
2014-04-26 14:51:21 +0200

    Miner progress

--- src/js/level/miner.js
+++ src/js/level/miner.js
@@ -1,4 +1,4 @@
-var Miner = function () {
+var Miner = function (angle, world, hotspot) {
 	var sprite = new PIXI.Sprite(PIXI.TextureCache[Utils.Assets.Images + 'level/characters/sprCharacterWalk.png']);
 
 	this.animations = {};
@@ -7,9 +7,10 @@ var Miner = function () {
 	extend(this, sprite);
 	extend(this.animations, AnimationManager());
 
+	this.hotspot = hotspot || 0;
 	this.collisionPoint = 265;
 	this.world = world;
-	this.angle = angle;
+	this.angle = angle || 0;
 	this.radius = this.collisionPoint;
 	this.velocity = 0;
 	this.position.x = 0;
@@ -25,6 +26,47 @@ var Miner = function () {
 	this.maxSpeed = 8;
 
 	this.jumpHeight = -10;
+
+	// AI variables
+	this.AI = {};
+	this.AI.currentState = null;
+	this.AI.currentTargetAngle = null;
+	this.AI.currentTargetStepsTaken = 0;
+	this.AI.distanceToTarget = function (bool) {
+		if (this.AI.currentTargetAngle !== null) {
+			if (bool) {
+				var distToTarget = this.angle - this.currentTargetAngle;
+			} else {
+				var distToTarget = this.angle > this.currentTargetAngle ? this.angle - this.currentTargetAngle : this.currentTargetAngle - this.angle;
+			}
+			return distToTarget;
+		}
+		return 0;
+	};
+	this.AI.calculateNextTarget = function () {
+		var distToHotspot = this.angle > this.hotspot ? this.angle - this.hotspot : this.hotspot - this.angle,
+			chance;
+
+		if (distToHotspot <= 20) {
+			chance = 90;
+		} else if (distToHotspot <= 40) {
+			chance = 70;
+		} else if (distToHotspot <= 60) {
+			chance = 50;
+		} else if (distToHotspot <= 80) {
+			chance = 20;
+		} else {
+			chance = 10;
+		}
+
+		if (Math.round(Math.random() * 100) <= chance) {
+			return (this.hotspot - 20) + Math.round(Math.random() * 40);
+		} else {
+			return (this.angle - 20) + Math.round(Math.random() * 40);
+		}
+
+		return 0;
+	};
 
 	var frames = [];
 
@@ -46,9 +88,76 @@ var Miner = function () {
         reversed: false
 	});
 
+	this.velocity = this.jumpHeight;
+	this.radius--;
+
 	this.update = function (data) {
 		this.animations.update(data);
+		this.health.updateHealthbar(data);
 
-		
+		if (this.AI.currentState !== null) {
+			switch(Math.round(Math.random() * 1)) {
+				case 0:
+					this.AI.currentState = 'idle';
+				break;
+				case 1:
+					this.AI.currentTargetAngle = this.AI.calculateNextTarget();
+					this.AI.currentState = 'targeted';
+				break;
+			}
+		} else if (this.AI.currentTargetAngle === this.angle) {
+			this.AI.currentTargetAngle = this.AI.calculateNextTarget();
+		}
+
+		if(this.AI.distanceToTarget() !== 0 && this.AI.distanceToTarget(true) <= 20) {
+			if(this.speed > 0)
+			{
+				this.speed = 0;
+			}
+			if (this.speed > -this.maxSpeed)
+			{
+				this.speed-=0.5;
+			}
+		}
+		else if(this.AI.distanceToTarget() !== 0 && this.AI.distanceToTarget(true) <= 20)
+		{
+			if(this.speed < 0)
+			{
+				this.speed = 0;
+			}
+			if (this.speed < this.maxSpeed)
+			{
+				this.speed+=0.5;
+			}
+		}
+		else
+		{
+			if(this.speed > 0)
+			{
+				this.speed-=0.5;
+			}
+			else if(this.speed < 0)
+			{
+				this.speed+=0.5;
+			}
+		}
+
+		if(this.speed == 0)
+		{
+			this.animations.pause("walk");
+		}
+		else
+		{
+			this.animations.resume("walk");
+		}
+
+		var wobble = Math.sin(this.angle*Math.PI/180*50);
+
+		this.position.x = this.world.position.x + Math.cos(this.angle * Math.PI / 180) * this.radius;
+		this.position.y = this.world.position.y + Math.sin(this.angle * Math.PI / 180) * (this.radius + wobble*4);
+
+		this.rotation = this.angle*Math.PI/180+Math.PI/2;
+
+		this.angle+=this.speed*data.dt;
 	};
 };
--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ require(
 		"level/world.js", 
 		"level/player.js",
 		"level/enemy.js",
+		"level/miner.js",
 		"level/healthbar.js",
 		"gameobject.js",
 		"level/cameracontroller.js"
@@ -15,21 +16,30 @@ function main()
     	world: null,
     	player: null,
     	enemies: [],
+    	miners: [],
     	players: [],
-    	hotspotMiners: Math.random() * 90,
-    	hotspotEnemies: Math.random() * 90 + 180,
 		name: "level",
 
 		initialise: function() {
+    		this.hotspotMiners = Math.random() * 90;
+    		this.hotspotEnemies = Math.random() * 90 + 180;
 			this.world = new World();
 			this.player = new Player(Math.random() * 360, this.world);
 
 			for (var i = 0; i < 10; ++i) {
-				this.enemies.push(new Enemy(Math.random() * 360, this.world));
+				this.miners.push(new Miner(Math.random() * 360, this.world, this.hotspotMiners))
+			}
+
+			for (var i = 0; i < 10; ++i) {
+				this.enemies.push(new Enemy(Math.random() * 360, this.world, this.hotspotEnemies));
 			}
 		},
 		update: function(data) {
 			this.player.update(data);
+			for (var i = 0; i < this.miners.length; ++i) {
+				this.miners[i].update(data);
+			}
+
 			for (var i = 0; i < this.enemies.length; ++i) {
 				this.enemies[i].update(data);
 			}
